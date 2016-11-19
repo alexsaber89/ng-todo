2acffe2d2879d1a70599318f536e549bec0a130c
Alex Saber <Alex Saber>
2016-11-19 11:38:44 -0600

    split index.html up into partials; now able to add and display tasks to and from firebase

--- javascripts/controllers/itemListCtrl.js
+++ javascripts/controllers/itemListCtrl.js
@@ -1,5 +1,19 @@
 "use strict";
 
-app.controller("ItemListCtrl", function($scope) {
-  $scope.message = "ItemListCtrl";
+app.controller("ItemListCtrl", function($scope, ItemFactory) {
+  $scope.items = [];
+
+  let getItems = function() {
+    ItemFactory.getItemList().then(function(fbItems) {
+      $scope.items = fbItems;
+    })
+  }
+
+  getItems();
+
+  $scope.deleteItem = function(itemID) {
+    ItemFactory.deleteItem(itemID).then(function(response) {
+      getItems();
+    })
+  }
 })
