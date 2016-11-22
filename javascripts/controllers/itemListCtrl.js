60c4e22773c68952e48946e4e8a1be2cec649270
Alex Saber <Alex Saber>
2016-11-21 18:53:37 -0600

    mvp achieved

--- javascripts/controllers/itemListCtrl.js
+++ javascripts/controllers/itemListCtrl.js
@@ -16,4 +16,9 @@ app.controller("ItemListCtrl", function($scope, ItemFactory) {
       getItems();
     })
   }
+
+  $scope.inputChange = function(thingy) {
+    ItemFactory.editItem(thingy).then(function(response) {
+    })
+  }
 })
